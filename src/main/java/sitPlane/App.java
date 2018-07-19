7a69571a804ded74c645d6128e7aca44ac0f7c47
david <david.garcia@pragsis.com>
2018-07-19 14:54:14 +0200

    changes

--- src/main/java/sitPlane/App.java
+++ src/main/java/sitPlane/App.java
@@ -1,20 +1,24 @@
 package sitPlane;
 
+import java.awt.Window;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class App {
 
+	private final static String windowValue = "W";
 	static List<String> alonesNotWindow = new ArrayList<String>();
 	static List<String> alonesWindow = new ArrayList<String>();
 
-	static List<String> groups = new ArrayList<String>();
+	static List<String[]> groups = new ArrayList<String[]>();
 
-	int[][] plane;
+	static int[][] plane;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -44,7 +48,6 @@ public class App {
 	public static void readFile(File file) {
 		// File fichero = new File("fichero_leer.txt");
 		Scanner s = null;
-		int[][] plane = null;
 		// Map <Integer, String []> plane = new HashMap<Integer, String[]>();
 		boolean itFirsLine = true;
 		Integer rows = null;
@@ -69,27 +72,24 @@ public class App {
 				} // queda por implementar que los datos obtenidos aqui sean validos.
 
 				else if (plane != null) {
-					// newPlane = new Plane(rows, seatsPerRow);
-					///
-					// List<String> alones = new ArrayList<String>();
+
+					// pasajeros solos
 					if (data.length == 1) {
 						System.out.println("pasajero solitario -" + data[0]);
-						alonesNotWindow.add(data[0]);
-						// leerFila(data);
+						boolean windowTrue = windowOrNot(data[0]);
+						clasified(data[0], windowTrue);
 					} else {
 						// procesar grupos
-						for (String passenger : data) {
-							System.out.println("pasajero solitario -" + data[0]);
-							groups.add(passenger);
+						groups.add(data);
 
-						}
 					}
 				}
 
-				// System.out.println(linea); // Imprimimos la linea
-				// leerFila(data);
-				// System.out.println(unidades);
 			}
+			System.out.println(groups.size());
+			System.out.println("recopilados pasajeros");
+
+			putPassengersInPlane();
 
 		} catch (Exception ex) {
 			System.out.println("Mensaje: " + ex.getMessage());
@@ -102,6 +102,68 @@ public class App {
 				System.out.println("Mensaje 2: " + ex2.getMessage());
 			}
 		}
+
+	}
+
+	private static void clasified(String data, boolean windowTrue) {
+		if (windowTrue) {
+			alonesWindow.add(extractPassergerForWindow(data));
+		} else {
+			alonesNotWindow.add(data);
+		}
+	}
+
+	private static String extractPassergerForWindow(String data) {
+		return data.substring(0, data.length() - 1);
+	}
+
+	private static boolean windowOrNot(String data) {
+		return (data.endsWith(windowValue)) ? true : false;
+
+	}
+
+	private static void putPassengersInPlane() {
+		// TODO Auto-generated method stub
+		// primero procesamos los grupos.
+		for (String[] group : groups) {
+			Map<String, String> clasified = new HashMap<String, String>();
+			for (String passenger : group) {
+				if (windowOrNot(passenger)) {
+					clasified.put(extractPassergerForWindow(passenger), windowValue);
+				} else {
+					clasified.put(passenger, "N");
+				}
+
+			}
+			System.out.println(clasified.values());
+			clasified.forEach((k, v) -> System.out.println("Key: " + k + ": Value: " + v));
+			if (clasified.containsValue(windowValue)) {
+				clasified.forEach((k, v) -> sitWithWindow(k, v));
+			} else {
+				clasified.forEach((k, v) -> sit(k, v));
+			}
+		}
+	}
+
+	private static Object sitWithWindow(String k, String v) {
+		// TODO Auto-generated method stub
+boolean lineIsOcuppied=true;
+Integer freeRow = null;
+for (int x=0;x<plane.length && lineIsOcuppied;x++) {
+	int[] row = plane[x];
+	System.out.println("huecos-"+row.length);
+}
+		if (v.equals(windowValue)) {
+
+		} else {
+
+		}
+		return null;
+	}
+	private static Object sit(String k, String v) {
+		// TODO Auto-generated method stub
+
+		return null;
 	}
 
 	private static void leerFila(String[] data) {
